176291a8dc4dec842a03950b33ffbafae3258d8b
sunnykhatter <lakshaykhatter@gmail.com>
2014-03-30 03:44:59 -0400

    Enhanced maps view and minor fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
-//= require foundation
+
 //= require turbolinks
+//= require foundation
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
